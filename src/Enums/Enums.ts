d2ec4f15b940e556774a427885d10fd062985989
IvanSavDev <ivan-savenko@mail.ru>
2022-10-13 21:30:18 +0300

    feat: add enums

---
+++ src/Enums/Enums.ts
@@ -0,0 +1,18 @@
+export enum FileFormats {
+  JSON = 'json',
+  YAML = 'yaml',
+}
+
+export enum ResultFormat {
+  Stylish = 'stylish',
+  Plain = 'plain',
+  JSON = 'json',
+}
+
+export enum TypesOfOperations {
+  Add = 'add',
+  Delete = 'delete',
+  Recursion = 'recursion',
+  Changed = 'changed',
+  Same = 'same',
+}
